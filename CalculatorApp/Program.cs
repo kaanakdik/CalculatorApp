c5a4c8e982d300847d6c956494dcdd6f2fbf1347
kaanakdik <kaanakdik@gmail.com>
2025-08-11 23:40:59 +0300

    toplama için üçüncü değişken eklendi

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -8,7 +8,10 @@ double x = double.Parse(Console.ReadLine());
 Console.Write("İkinci sayıyı girin: ");
 double y = double.Parse(Console.ReadLine());
 
-Console.WriteLine($"Toplama: {calc.Add(x, y)}");
+Console.Write("Üçüncü sayıyı girin: ");
+double z = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Toplama: {calc.Add(x, y, z)}");
 Console.WriteLine($"Çıkarma: {calc.Subtract(x, y)}");
 Console.WriteLine($"Çarpma: {calc.Multiply(x, y)}");
 Console.WriteLine($"Bölme: {calc.Divide(x, y)}");
