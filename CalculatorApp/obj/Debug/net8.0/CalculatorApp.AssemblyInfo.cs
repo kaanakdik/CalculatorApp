f6a0f00e1124f6dc15071a1654d2a38a41d035cd
kaanakdik <kaanakdik@gmail.com>
2025-08-11 23:26:40 +0300

    Uygulama için Jenkinsfile dosyası eklendi

--- CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
+++ CalculatorApp/obj/Debug/net8.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcbb118d68d0fccba9caa87e07630635e33f7110")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8437189582d48d94cfa13ee5dea02d711cf983c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
